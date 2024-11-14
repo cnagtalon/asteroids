1659b47e0fae36aff954a6146795c5e4247abe41
Christian Nagtalon <christalon@ChrisTalon.local>
2024-11-14 17:13:24 -0600

    Added rotation and movement to player

--- main.py
+++ main.py
@@ -14,20 +14,33 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
 
     # Create the player before the game loop
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+
     #the game loop
     while True:
-        screen.fill(BLACK)
-        player.draw(screen)
-        pygame.display.flip()
+        dt = (clock.tick(60) /1000)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        dt = (clock.tick(60) /1000)
+            
+        for sprite in updatable:
+            sprite.update(dt)
+            
+        screen.fill(BLACK)
+        for sprite in drawable:
+            sprite.draw(screen)
+        pygame.display.flip()
+
+
 
 
 
