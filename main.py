fef2b62bc03bcad7a41fe684b10dc6ae69765c30
Christian Nagtalon <christalon@ChrisTalon.local>
2024-11-14 14:17:06 -0600

    added player triangle

--- main.py
+++ main.py
@@ -2,24 +2,34 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, BLACK
+from player import Player
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+    # Create the player before the game loop
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    #the game loop
     while True:
-        screen.fill(black)
+        screen.fill(BLACK)
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = (clock.tick(60) /1000)
 
-black = (0,0,0)
+
 
 
 
